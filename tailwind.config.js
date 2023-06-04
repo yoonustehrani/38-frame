9f7cac87d373d01870714c266342ed08f13d84fa
yoonus <yoonustehrani28@gmail.com>
2023-06-04 16:03:13 +0330

    all js dependencies + tailwind + postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,26 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        // 'appear-from-right': 'AppearFromRight linear 750ms',
+      },
+      fontFamily: {
+        'iran-sans': 'IranSansWeb'
+      },
+      colors: {
+        'x-green': '#558564',
+        'x-dark-blue': '#102542',
+        'x-yellow': '#FE9920',
+        'x-grey': '#CDD7D6',
+        'custom-black': {
+          900: '#222222',
+          800: '#333333',
+          DEFAULT: '#000000'
+        }
+      }
+    },
   },
-  plugins: [],
+  plugins: []
 }
 
