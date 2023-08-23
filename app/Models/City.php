adcc897529d8ee5863b29908efe0ac217a3415b3
yoonus <yoonustehrani28@gmail.com>
2023-08-24 00:15:06 +0330

    province relationship for City model

--- app/Models/City.php
+++ app/Models/City.php
@@ -10,4 +10,9 @@ class City extends Model
     use HasFactory;
     protected $fillable = ['name', 'province_id'];
     public $timestamps = false;
+
+    public function province()
+    {
+        return $this->belongsTo(Province::class);
+    }
 }
