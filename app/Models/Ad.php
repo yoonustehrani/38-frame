c757146635d20fb37e51db9df076688fbc754633
yoonus <yoonustehrani28@gmail.com>
2023-08-24 00:16:53 +0330

    datetime casting for two fields in Ad model

--- app/Models/Ad.php
+++ app/Models/Ad.php
@@ -5,11 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Ad extends Model
 {
-    use HasFactory, HasUlids;
-
+    use HasFactory, HasUlids, SoftDeletes;
+    protected $casts = [
+        'expires_at' => 'datetime',
+        'published_at' => 'datetime'
+    ];
     public function publisher()
     {
         return $this->morphTo();
