e9e39dc95da46096e986babc0996de2817443d60
yoonus <yoonustehrani28@gmail.com>
2023-08-26 08:29:59 +0330

    translating model related strings in lang dir

---
+++ lang/fa/models.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'ads' => [
+        'status' => [
+            'awaiting_confirmation' => 'در انتظار برای تایید',
+            'rejected' => 'رد شده',
+            'confirmed' => 'تایید شده',
+            'expired' => 'منقضی شده'
+        ]
+    ]
+];
