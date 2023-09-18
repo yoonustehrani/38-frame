584d6ad81eecd6071385ca3818add072686fcd64
yoonus <yoonustehrani28@gmail.com>
2023-09-18 16:25:41 +0330

    shop api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ShopController;
 use App\Models\SiteCategory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -26,12 +27,14 @@ Route::middleware('auth:sanctum')->group(function() {
             return $request->user();
         });
         Route::get('/shop', function(Request $request) {
-            return $request->user()->shop()->first();
+            return $request->user()->shop()->firstOrFail();
         });
         Route::patch('settings/social', function(Request $request) {
             return \App\Models\User::latest()->first();
         });
     });
+    Route::apiResource('shops', ShopController::class)->only(['destroy', 'store', 'update']);
+    // Route::post('labs/register', [ShopController::class, 'store'])->name('labs.store');
 });
 
 Route::post('auth/google', 'App\Http\Controllers\AuthController@handleGoogleSignIn');
