1f44fdd79c078e276be2e5a84938daa5d51b9598
yoonus <yoonustehrani28@gmail.com>
2023-10-12 20:13:38 +0330

    new api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\LabController;
 use App\Http\Controllers\ShopController;
+use App\Http\Resources\CityResource;
+use App\Models\City;
 use App\Models\SiteCategory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,8 +21,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function() {
     Route::apiResource('ads', 'App\Http\Controllers\AdController');
-    Route::get('site-categories', function() {
-        return SiteCategory::whereType(null)->get();
+    Route::get('site-categories', function(Request $request) {
+        return SiteCategory::whereType(
+            $request->query('type') ?: null
+        )->get();
     })->name('site-categories.index');
     
     Route::prefix('user')->name('user.')->group(function() {
@@ -33,9 +38,23 @@ Route::middleware('auth:sanctum')->group(function() {
             return \App\Models\User::latest()->first();
         });
     });
+    Route::apiResource('shops/{shop}/merchandise', ShopController::class);
     Route::apiResource('shops', ShopController::class)->only(['destroy', 'store', 'update']);
+    Route::post('auth/logout', 'App\Http\Controllers\AuthController@logout');
     // Route::post('labs/register', [ShopController::class, 'store'])->name('labs.store');
 });
-
+Route::get('labs', [LabController::class, 'index']);
+Route::get('cities', function(Request $request) {
+    $request->validate([
+        'query' => 'required|string|min:2'
+    ]);
+    return CityResource::collection(
+        \App\Models\City::with('province')->where('name', 'like', "{$request->query('query')}%")->orderBy('name')->limit(4)->get()
+    );
+});
+Route::get('cities/{city}', function($city) {
+    return (new CityResource(City::with('province')->findOrFail($city)));
+});
+Route::apiResource('lab-services', 'App\Http\Controllers\LabServiceController');
 Route::post('auth/google', 'App\Http\Controllers\AuthController@handleGoogleSignIn');
 Route::post('auth/login', 'App\Http\Controllers\AuthController@login');
