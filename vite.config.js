7c7a533204947fb3f7b21a6033c743ab23b53fdd
yoonus <yoonustehrani28@gmail.com>
2023-10-16 09:50:11 +0330

    production of js and css

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
                 'resources/js/react-app.tsx',
-                'resources/js/react-auth.tsx',
+                // 'resources/js/react-auth.tsx',
                 'resources/js/react-userarea.tsx'
             ],
             refresh: true,
