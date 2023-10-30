7eb05ac5d032e039efc9844103a2daa02c05284f
yoonus <yoonustehrani28@gmail.com>
2023-10-30 21:00:37 +0330

    Store lab request is done

--- app/Http/Requests/StoreLabRequest.php
+++ app/Http/Requests/StoreLabRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Lab;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
 
 class StoreLabRequest extends FormRequest
 {
@@ -11,7 +13,7 @@ class StoreLabRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return $this->user()->can('create', Lab::class);
     }
 
     /**
@@ -22,7 +24,27 @@ class StoreLabRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'brand' => 'required|string|min:3|max:40',
+            'active' => 'nullable',
+            'founded_in_year' => 'required|integer|digits:4',
+            'bio' => 'required|string|min:6|max:300',
+            'address' => 'required_if:in_person_sale,yes|string|min:10|max:150',
+            'category' => 'required|string',
+            'phone_number' => 'required|string|numeric|digits:11|regex:/09[0-9]{9}/',
+            'address_line' => 'nullable|string|numeric',
+            'meta.social.instagram' => 'nullable|string|min:4|max:60',
+            'meta.social.telegram_channel' => 'nullable|string|min:4|max:60',
+            'iban' => 'required|string|numeric|digits:24',
+            'owner_fullname' => 'required|string|min:3|max:60',
+            'owner_national_id' => 'required|string|numeric|digits:10',
+            'website' => 'nullable|string|url',
+            'meta.workingDays' => 'required', // TODO
+            'meta.onlySms' => 'nullable',
+            'services.*.service_id' => 'required|integer',
+            'services.*.description' => 'required|string|min:3|max:200',
+            'services.*.price' => 'nullable|integer|min:1000',
+            'services.*.price_note' => 'nullable|string|min:1|max:50',
+            'accpect_policy' => 'required|accepted'
         ];
     }
 }
