cc4cf3139a87ed3fe209fc7c8f726befe6a05996
yoonus <yoonustehrani28@gmail.com>
2023-12-07 09:21:35 +0330

    cleared out useless comments

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    // return get_menu('main');
     $roadmap = [
         [
             'title' => 'تهیه دوربین آنالوگ',
