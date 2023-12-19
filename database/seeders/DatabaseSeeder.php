6d7fb732d98c056081fbec4bce76c7d1859b6d8c
yoonus <yoonustehrani28@gmail.com>
2023-12-19 15:29:00 +0330

    Filled database seeder with initial seeders to load when refreshing database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,5 +18,13 @@ class DatabaseSeeder extends Seeder
         $this->call(ProvinceSeeder::class);
         $this->call(CitySeeder::class);
         $this->call(SiteCategorySeeder::class);
+        $this->call(MenuSeeder::class);
+        $this->call(RoleSeeder::class);
+
+        $user = User::factory()->make();
+        $user->email = '38frame@gmail.com';
+        $user->password = bcrypt('hello1234');
+        $user->role_id = Role::whereName('admin')->firstOrFail()?->id;
+        $user->save();
     }
 }
